f4729aef49f6f86436bfc7a43044b30de04c6b13
Paul Tunbridge <tunbridgep@gmail.com>
2022-07-16 19:02:42 +1000

    More organisation. Fixed make.cmd

--- make.cmd
+++ make.cmd
@@ -1,2 +1,4 @@
 @echo off
-build/makedml.cmd 1 ::Generate DML headers
+::Don't forget quotes when specifying paths!
+::Passing 1 as the third parameter will generate DML headers
+build/makedml.cmd "%~dp0\src" "%~dp0\out" 1
