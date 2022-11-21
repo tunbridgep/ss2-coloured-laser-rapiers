90b572162a6799ea069cbb39660ef948735b28b0
Paul Tunbridge <tunbridgep@gmail.com>
2022-11-21 06:52:46 -0800

    Updated Make to be compatible with the new makedml system

--- make.cmd
+++ make.cmd
@@ -1,5 +1,7 @@
 @echo off
 ::Don't forget quotes when specifying paths!
 ::Passing 1 as the fourth parameter will generate DML headers
-call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" 1
+call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp" "secmod"
+@del "Coloured Laser Rapiers AIO.7z"
+7z a "Coloured Laser Rapiers AIO.7z" "%~dp0\out\*"
 pause
