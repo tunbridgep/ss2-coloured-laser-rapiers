09792bb2c859e224b4ac1e133176ad03af64a0e5
Paul Tunbridge <tunbridgep@gmail.com>
2023-07-22 19:40:11 +1000

    Updated build script

--- make.cmd
+++ make.cmd
@@ -1,7 +1,8 @@
 @echo off
+::DO NOT USE ! OR % IN YOUR FOLDER NAMES!!!!
+::IT WILL BREAK THE SCRIPT!
 ::Don't forget quotes when specifying paths!
-::Passing 1 as the fourth parameter will generate DML headers
+::Use -f for feature mode, -v for version mode
+::Pass additional parameters to add DML headers as specified in the headers folder (feature mode only for now)
 call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp" "secmod"
-@del "Coloured Laser Rapiers All-in-One.7z"
-7z a "Coloured Laser Rapiers All-in-One.7z" "%~dp0\out\*"
-pause
+call build/makedml.cmd -z -f "%~dp0\out" "Coloured Laser Rapiers AIO 1.05"
